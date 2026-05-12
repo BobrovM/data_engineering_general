391b8169b43596d8078a719cf140ee863632f092
OFFICEGUYLPTP\bobrt <bobrtvlol@gmail.com>
2026-05-12 16:12:37 +0300

    Now with API supported pagination and a bit bigger amount of data from API. Original API is still inaccessible.

--- step_01_data_sourcing/data_sourcer_task/package_data_sourcer_task/api_data_sourcer.py
+++ step_01_data_sourcing/data_sourcer_task/package_data_sourcer_task/api_data_sourcer.py
@@ -15,7 +15,6 @@ import httpx
 import os.path 
 import asyncio # gotta learn the asyncio lib
 import csv
-# from csvkit import convert
 from dotenv import load_dotenv
 
 # dotenv was added through VSCode copilot, had troubles with accessing system env vars and then .env, asked LLM/AI,
@@ -25,6 +24,7 @@ load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '../../.env'))
 
 ### Looks like pirated course API got shut down before 2026.05.05, or it is temp unavailable at this moment, idk.
 ### 2026.05.04 it was fine. Changing project to CoinGecko MR Beast cryptoscams for fun and funny training. But it's not 2 gigs...
+### Opened on 2026.05.11, it was fine. 2026.05.12 it is dead.
 """
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -34,17 +34,11 @@ with open('{0}/api_url.txt'.format(dir_path), 'r') as file:
 """
 
 api_key = os.getenv('CoinGecko_OfficeGuy_API')
-address = "https://api.coingecko.com/api/v3/coins/list"
+#address = "https://api.coingecko.com/api/v3/coins/list"
+address = "https://api.coingecko.com/api/v3/coins/markets"
 header = {"x-cg-demo-api-key": api_key}
 
-### test to see that I did not f-up the address
-# print(address)
-# print(header)
-
-
 # async def was AI suggestion, had troubles with await, I need to study asyncio, interested if each response is a unique callback
-# will it be an unsorted mess????
-# can it bug out and make a duplicate?????
 # and 5 minutes after I did it i found the solution in pdf files of the pirated courses
 # TODO needs pagination, learn pagination
 async def api_data_receiver():
@@ -56,26 +50,76 @@ async def api_data_receiver():
     Even this is a try of doing asynchronous code, I need more understanding of how does it work.
     """
     # Force UTF-8, got UnicodeEncodeError: 'charmap' codec can't encode character '\u0f3c' in position 6: character maps to <undefined>
-    with open('final_data.csv', 'w', encoding='utf-8') as file:    
-        fieldnames = ["id", "symbol", "name"]
+    with open('coingecko_coins_markets_data.csv', 'w', encoding='utf-8') as file:
+        #fieldnames = ["id", "symbol", "name"]
+        fieldnames = [
+            'id',
+            'symbol',
+            'name',
+            'image',
+            'current_price',
+            'market_cap',
+            'market_cap_rank',
+            'fully_diluted_valuation',
+            'total_volume',
+            'high_24h',
+            'low_24h',
+            'price_change_24h',
+            'price_change_percentage_24h',
+            'market_cap_change_24h',
+            'market_cap_change_percentage_24h',
+            'circulating_supply',
+            'total_supply',
+            'max_supply',
+            'ath',
+            'ath_change_percentage',
+            'ath_date',
+            'atl',
+            'atl_change_percentage',
+            'atl_date',
+            'roi',
+            'last_updated'
+        ]
+
         writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter="\t")
         writer.writeheader()
 
 
         async with httpx.AsyncClient() as client:
             tries = 0
+            # in case original API will go back up. But it is not known if it clearly has pagination params.
+            page = 1
+
             while True:
                 try:
                     # Getting cryptoscams lists
-                    # print(httpx.get(address, headers=header).text)
-                    response = await client.get(address, headers=header)
+                    # params will work if API has obvious page and per_page. When the needed API will go up, we will see
+                    # and fix all that
+                    q_params = {
+                        "page": page,
+                        "per_page": 200,
+                        "vs_currency": "usd"
+                    }
+
+                    response = await client.get(address, headers=header, params=q_params)
                     response.raise_for_status()
 
                     json_response = response.json()
 
-                    for row in json_response:
-                        writer.writerow(row)
-                    break
+                    print(page)
+                    print(json_response)
+
+                    if not json_response:
+                        print("Pages ended")
+                        break
+
+                    #for row in json_response:
+                    writer.writerows(json_response)
+
+                    # increase page and reset tries
+                    page += 1
+                    tries = 0
+
 
                 except httpx.TimeoutException:
                     print("uhoh")
