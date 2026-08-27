562c1368d3545ae6ddffb5a7e72a5afc2b9e6a9f
OFFICEGUYLPTP\bobrt <bobrtvlol@gmail.com>
2026-08-27 14:28:05 +0300

    Full map reduce pipeline is now finished.

--- step_06_airflow/dags/customs_enriched.py
+++ step_06_airflow/dags/customs_enriched.py
@@ -41,10 +41,6 @@ def _check_mdate():
         return 'task_no_update'
 
 
-'''def _enrich_mapreduce():
-    pass'''
-
-
 with DAG(
     'customs_enriched',
     start_date=datetime(2026, 8, 1),
@@ -71,10 +67,91 @@ with DAG(
         """,
     )
 
-    '''
-    enrich_mapreduce = PythonOperator(
-        task_id='enrich_mapreduce',
-        python_callable=_enrich_mapreduce,
-    )'''
+    run_mapreduce_codedescs = SSHOperator(
+        task_id='run_mapreduce_codedescs',
+        ssh_conn_id='hdfs-namenode-ssh',
+        command="""
+        echo "Hostname: $(hostname)"
+ 
+        # SetEnvVars
+        export JAVA_HOME=/opt/java/openjdk
+        export HADOOP_HOME=/opt/hadoop
+        export HADOOP_CONF_DIR=/opt/hadoop/etc/hadoop
+        export YARN_HOME=/opt/hadoop
+        export HADOOP_LOG_DIR=/var/log/hadoop
+        export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
+        
+        echo "JAVA_HOME: $JAVA_HOME"
+        echo "HADOOP_HOME: $HADOOP_HOME"
+        echo "HADOOP_CONF_DIR: $HADOOP_CONF_DIR"
+        echo "YARN_HOME: $YARN_HOME"
+        echo "PATH: $PATH"
+        
+        # Magic
+        # idk why do i use full path but let it be
+        /opt/hadoop/bin/hdfs dfs -rm -r -f /task1/output_t1
+        bash /scripts/shell_scripts_codedescs/yarn_step1_v4.sh
+        /opt/hadoop/bin/hdfs dfs -ls /task1/output_t1
+        """
+    )
+
+    run_mapreduce_history = SSHOperator(
+        task_id='run_mapreduce_history',
+        ssh_conn_id='hdfs-namenode-ssh',
+        command="""
+        echo "Hostname: $(hostname)"
+ 
+        # SetEnvVars
+        export JAVA_HOME=/opt/java/openjdk
+        export HADOOP_HOME=/opt/hadoop
+        export HADOOP_CONF_DIR=/opt/hadoop/etc/hadoop
+        export YARN_HOME=/opt/hadoop
+        export HADOOP_LOG_DIR=/var/log/hadoop
+        export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
+        
+        echo "JAVA_HOME: $JAVA_HOME"
+        echo "HADOOP_HOME: $HADOOP_HOME"
+        echo "HADOOP_CONF_DIR: $HADOOP_CONF_DIR"
+        echo "YARN_HOME: $YARN_HOME"
+        echo "PATH: $PATH"
+        
+        # Magic
+        # idk why do i use full path but let it be
+        /opt/hadoop/bin/hdfs dfs -rm -r -f /task1/output_t2
+        bash /scripts/shell_scripts_history_csv/yarn_step2_v2.sh
+        /opt/hadoop/bin/hdfs dfs -ls /task1/output_t2
+        """
+    )
+
+    run_mapreduce_lazysort = SSHOperator(
+        task_id='run_mapreduce_lazysort',
+        ssh_conn_id='hdfs-namenode-ssh',
+        command="""
+        echo "Hostname: $(hostname)"
+ 
+        # SetEnvVars
+        export JAVA_HOME=/opt/java/openjdk
+        export HADOOP_HOME=/opt/hadoop
+        export HADOOP_CONF_DIR=/opt/hadoop/etc/hadoop
+        export YARN_HOME=/opt/hadoop
+        export HADOOP_LOG_DIR=/var/log/hadoop
+        export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
+        
+        echo "JAVA_HOME: $JAVA_HOME"
+        echo "HADOOP_HOME: $HADOOP_HOME"
+        echo "HADOOP_CONF_DIR: $HADOOP_CONF_DIR"
+        echo "YARN_HOME: $YARN_HOME"
+        echo "PATH: $PATH"
+        
+        # Magic
+        # idk why do i use full path but let it be
+        /opt/hadoop/bin/hdfs dfs -rm -r -f /task1/output_lazysort
+        bash /scripts/shell_scripts_lazysort/yarn_step_lazysort_v4.sh
+        /opt/hadoop/bin/hdfs dfs -ls /task1/output_lazysort
+        /opt/hadoop/bin/hdfs dfs -get /task1/output_lazysort /scripts/results_from_hdfs_lazysort_FINAL_OUTPUT
+        """
+    )
+
 
 check_mdate >> [task_no_update, put_into_hdfs]
+put_into_hdfs >> run_mapreduce_codedescs >> run_mapreduce_history >> run_mapreduce_lazysort
